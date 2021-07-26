19177ea1c5ba8c9f3bbbf85e3e29bcbd64aa68ed
amintado <amintado@gmail.com>
2021-07-27 02:14:34 +0430

    LEAR-28 Teachers excel parse

---
+++ back/dynamicServer/src/helpers/ExcelParser.js
@@ -0,0 +1,39 @@
+/*
+ *
+ *  * Copyright (c) 2021. Learniba Project, Interactive Education Platform
+ *  * Programmed By: YiiMan amintado@gmail.com 09353466620
+ *  * Author: Learniba TM
+ *  *
+ *
+ */
+import {StreamUtils } from 'xlsx'
+import XLSX from 'xlsx'
+export default class {
+  file;
+  workbook;
+  sheet;
+
+  read_file(){
+    this.workbook=XLSX.readFile(this.file);
+  }
+
+  constructor(filename) {
+    this.file = filename;
+    this.read_file();
+  }
+
+  /**
+   *
+   * @param index
+   * @param {array< object> } headers
+   */
+  load_sheet(index,headers=[]) {
+    this.sheet= this.workbook.Sheets[this.workbook.SheetNames[index]];
+    XLSX.utils.sheet_to_json({
+      headers:headers
+    })
+  }
+
+
+
+}
